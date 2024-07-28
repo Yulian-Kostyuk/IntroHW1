ac5af38ad8110f1317487873848745a74251dd5b
Yulian-Kostyuk <122096456+Yulian-Kostyuk@users.noreply.github.com>
2024-07-28 14:44:09 +0300

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,27 @@
+# 1st program
+print(9 ** 0.5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(((1234%1000)//10)+((5678%1000)//10))
+
+#4th program
+print()
+a = 13.42
+b = 42.13
+c = 100
+print(a)
+print(a * c)
+print(a * c % c)
+print()
+print(int(a))
+print(int(a * c % c))
+print()
+print(int(b))
+print(int(b * c % c))
+print()
+print(int(b) == int(a * c % c))
+print(int(a) == int(b * c % c))
+print(int(b) == int(a * c % c) or int(a) == int(b * c % c))
